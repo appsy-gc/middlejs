8b5acfafa4ea70c63c5472b3d1cb6dcf8fe2bf90
Chris Apps <chrisapps@Chriss-Mac-mini.local>
2025-01-27 08:03:24 +1000

    Mongoose db set up

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 // Import the ExpressJS Package
 const express = require('express')
+const mongoose = require('mongoose')
 
 // Create an instance of Express
 const app = express()
 
+// Enable ability to send data to the server
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// Declare routes
+
+
 // Declare the port number
 const port = 3000
 
-// Simple get request to get started
-app.get('/', (req, res) => {
-    response.send('Hello there')
+// Start listening for traffic
+app.listen(port, async () => {
+    console.log(`Server started on port ${port}`)
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/testie_db')
+        console.log('Connected to database')
+    } catch (error) {
+        console.log(`Database failed to connect. ${JSON.stringify(error)}`)
+    }
+    
 })
 
-// Start listening for traffic
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
+// HTML test route with middleware
+app.get('/', messageWithVerb, (req, res) => {
+    res.send('This is the home page')
 })
+
+// Function for DRY routes
+function messageWithVerb(req, res, next) {
+    console.log(`Received a request with the ${req.method} HTTP verb...`)
+    next()
+}
